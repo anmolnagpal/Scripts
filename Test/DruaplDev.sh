38d25063e4bf6284b335ff00c89e3aaca89b43d4
Anmol Nagpal <anmolnagpal@tweakntip.com>
2015-08-07 00:35:09 +0530

    Update

--- Test/DruaplDev.sh
+++ Test/DruaplDev.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
-# Shell script to install All  dependencies for Yellow Obejects
+# Shell script to install All  dependencies for 
 #		"Developer Machine" 			
 # -------------------------------------------------------------------------
-# Version 1.0 (May 10 2015)
+# Version 1.0 (Sep 24 2013)
 # -------------------------------------------------------------------------
 # Copyright (c) 2013 Anmol Nagpal <http://www.tweakntip.in>
 # This script is licensed under GNU GPL version 2.0 or above
 # -------------------------------------------------------------------------
-
-#Tools Installed
-# Adduser (Admin)
-# Apache2 
-# PHP5
-# MySQL
-# phpMyAdmin
-# Java
-# Browser (Opera Safari(Wine) chromium Chrome)
-# Teamviewer
-# Skype
-# Elance Tracker
-# Firewall Disable
-# Other's (vim, VLC, Xpad, geany, Shutter ) 
 
 if ! [ $(id -u) = 0 ]; then
    echo "Please login via root!"
@@ -69,6 +55,18 @@ else
 
 
 fi
+
+# Git Tool's
+sudo apt-get install gitg
+
+# Version Control SVN
+apt-get install subversion -y
+
+# Version Control GIT
+apt-get install git -y
+apt-get install gitg -y
+apt-get install gitk -y
+
 # Apache 2
 apt-get install -y apache2 libapache2-mod-xsendfile apache2-mpm-itk
 a2enmod rewrite
@@ -77,37 +75,36 @@ service apache2 restart
 # PHP 5
 apt-get install -y php5 php5-curl php5-intl php5-mcrypt php5-memcache php5-memcached php5-devel php5-mysql php5-gd php5-xdebug
 
-# MySQL
-echo mysql-server-5.1 mysql-server/root_password password root| debconf-set-selections
-echo mysql-server-5.1 mysql-server/root_password_again password root| debconf-set-selections
-sudo apt-get install -y mysql-server
-
 # Firewall Disabled
-ufw disable	
+service ufw disable			
 
-# Skype
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-sudo apt-get update -y
-sudo apt-get install skype -y
+# Other utils
+apt-get install -y coffeescript ruby-compass
+apt-get install gnome-system-tools -y
+apt-get install dconf-tools -y
+apt-get install ssh -y
+apt-get install filezilla -y #ftp client
+apt-get install htop -y # network tool
+apt-get install vim -y # editor
+apt-get install diffuse -y # compare file
+apt-get install bmon -y
+apt-get install meld -y # compare tool
 
 # JAVA
 apt-get install openjdk-7-jre -y
 apt-get install openjdk-6-jre -y
 
 # Browser
-apt-get install chromium-browser -y
-
+apt-get install chromium-browser -
 # Google-Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
-
 ##Opera
 sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
 sudo sh -c 'wget -O - http://deb.opera.com/archive.key | apt-key add -'
 sudo apt-get update && sudo apt-get install opera -y
-
 #Safari
 # install wine
 sudo apt-get install -y wine 
@@ -119,13 +116,23 @@ wget http://appldnld.apple.com/Safari5/041-5487.20120509.INU8B/SafariSetup.exe
 # wine
 wine SafariSetup.exe
 
-#Other Tools
-sudo apt-get install xpad -y # Xpad
-sudo apt-get install vim -y # Editor
-sudo apt-get install shutter -y # Screen Shot Tool
-sudo apt-get install geany -y #Editor
-sudo apt-get install filezilla # FTP
-sudo apt-get install vlc -y # Player
+# MySQL
+echo mysql-server-5.1 mysql-server/root_password password root| debconf-set-selections
+echo mysql-server-5.1 mysql-server/root_password_again password root| debconf-set-selections
+sudo apt-get install -y mysql-server
+
+# Skype
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+sudo apt-get update
+sudo apt-get install skype -y
+
+
+# phpMyAdmin
+sudo apt-get install phpmyadmin -y 
+
+#htop network monitoring tool for Developer Machine # via http://ipaddress:3000
+sudo apt-get install ntop -y 
+sudo /etc/init.d/ntop -i eth0 start
 
 ##Teamviewer
 # install required libs
@@ -161,24 +168,6 @@ sudo apt-get install -f -y
 # clean up
 rm /tmp/teamviewer_*.deb
 
-#Elance Tracket
-apt-get install libgtk2.0-0:i386 libstdc++6:i386 libnss3-1d:i386 lib32nss-mdns libxml2:i386 libxslt1.1:i386 libcanberra-gtk-module:i386 gtk2-engines-murrine:i386
-cd /tmp
-apt-get download libgnome-keyring0:i386
-dpkg-deb -R libgnome-keyring0_3.8.0-2_i386.deb gnome-keyring
-cp gnome-keyring/usr/lib/i386-linux-gnu/libgnome-keyring.so.0.2.0 /usr/lib/i386-linux-gnu/
-ln -s /usr/lib/i386-linux-gnu/libgnome-keyring.so.0.2.0 /usr/lib/i386-linux-gnu/libgnome-keyring.so.0
-ln -s /usr/lib/i386-linux-gnu/libgnome-keyring.so.0 /usr/lib/libgnome-keyring.so.0
-ln -s /usr/lib/i386-linux-gnu/libgnome-keyring.so.0.2.0 /usr/lib/libgnome-keyring.so.0.2.0
-wget http://airdownload.adobe.com/air/lin/download/2.6/AdobeAIRInstaller.bin
-chmod a+x AdobeAIRInstaller.bin
-./AdobeAIRInstaller.bin
-rm ./AdobeAIRInstaller.bin
-rm /usr/lib/libgnome-keyring.so.0
-rm /usr/lib/libgnome-keyring.so.0.2.0
-wget https://www.elance.com/tracker/TrackerSetup.deb
-dpkg -i TrackerSetup.deb
 
-## phpMyAdmin
-sudo apt-get  install phpmyadmin
-fi
+# Upgrade Ubuntu
+sudo apt-get upgrade -y
